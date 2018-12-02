712cc38ac88152db5f42631f101c753c0ff44210
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-02 15:44:42 -0800

    Added put endpoint and error handling

--- server.js
+++ server.js
@@ -105,6 +105,38 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+// Put endpoint
+// Verify item has required fields
+// Verify item ID in updated item object matches endpoint url
+// Log errors for both
+// Otherwise call `ShoppingList.update` with updated item
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'id', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++){
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${request.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating shopping list item \`${req.params.id}`);
+  Recipes.update({
+    name: req.body.name,
+    id: req.params.id,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).end();
+});
+
 app.delete('/recipes/:id', (req, res) => {
   Recipes.delete(req.params.id);
   console.log(`Deleted recipe \`${req.params.ID}\``);
